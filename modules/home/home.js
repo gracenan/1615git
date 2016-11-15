d9199450528695fb371a80442bafe498ced4cbef
xiaonan <869043076@qq.com>
2016-11-15 17:36:26 +0800

    a

--- modules/home/home.js
+++ modules/home/home.js
@@ -89,8 +89,7 @@ define(['text!./home.html','css!./home.css'],function(html){
     
     	$("#saoyisao").click(function(){
     	
-    		alert('------')
-    		
+//  		alert('------')
     		wx.scanQRCode({
 		    needResult: 0, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
 		    scanType: ["qrCode","barCode"], // 可以指定扫二维码还是一维码，默认二者都有
@@ -98,6 +97,7 @@ define(['text!./home.html','css!./home.css'],function(html){
 		    var result = res.resultStr; // 当needResult 为 1 时，扫码返回的结果
 		}
 		});
+
 
     	})
 //		$("header_home_title").children(0).on("touchstart",function(){
